e58cd7ed6532f911026c11b50bb5557541084bff
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2022-07-02 17:54:21 +0530

    solved fifth question

--- fifth.js
+++ fifth.js
@@ -1,1 +1,14 @@
 // Uppercase of each first letter of a words using Array map function
+
+let str = " Hi, this is my country - India. ";
+function upperCaseFirsstLetter(str) {
+  str = str.trim().split(" ");
+  let res = str.map(function elem(item) {
+    let first = item[0].toUpperCase();
+    first = item.replace(item[0], first);
+    return first;
+  });
+  res = res.join(" ");
+  return res;
+}
+console.log(upperCaseFirsstLetter(str));
--- fourth.js
+++ fourth.js
@@ -1,4 +1,18 @@
 // Uppercase of each first letter of a words.
+
+/**
+ * Step 01 - Create a function - Uppercase.
+ * Step 02 - Now split the string to make it array.
+ * Step 03 - Make an iteration on the array.
+ * Step 04 - Check for a if condition that the current index is a space and next index has ascii code greater than 96.
+ * Step 05 - If yes than check tha ascii code of that number.
+ * Step 06 - Replace that cahracter with uppercase character by using ascii code.
+ * Step 07 - Now join the array to make it string again.
+ * Step 08 - A exceptional should be handle before Step 03.
+ * Step 09 - That to check the first character is uppercase or lowercase.
+ * Step 10 - If it is lower case than replace it with uppercase.
+ */
+
 let str = "my name is ritesh.";
 
 function Uppercase(str) {
